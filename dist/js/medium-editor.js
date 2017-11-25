42f68307b03e32405f2b28cc031d53a6103aef2a
Asad Dhamani <dhamaniasad@gmail.com>
2017-11-25 21:12:26 +0530

    Update build

--- dist/js/medium-editor.js
+++ dist/js/medium-editor.js
@@ -385,16 +385,16 @@ if (!("classList" in document.createElement("_"))) {
 
 (function (root, factory) {
     'use strict';
-    var isElectron = typeof module === 'object' && typeof process !== 'undefined' && process && process.versions && process.versions.electron;
-    if (!isElectron && typeof module === 'object') {
-        module.exports = factory;
-    } else if (typeof define === 'function' && define.amd) {
-        define(function () {
-            return factory;
-        });
-    } else {
+//     var isElectron = typeof module === 'object' && typeof process !== 'undefined' && process && process.versions && process.versions.electron;
+//     if (!isElectron && typeof module === 'object') {
+//         module.exports = factory;
+//     } else if (typeof define === 'function' && define.amd) {
+//         define(function () {
+//             return factory;
+//         });
+//     } else {
         root.MediumEditor = factory;
-    }
+//     }
 }(this, function () {
 
     'use strict';
